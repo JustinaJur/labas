b1ccb9323c714014be468002666873cfc69b2236
JustinaJur <jurgeleviciute.justina@gmail.com>
2024-10-29 14:53:08 +0200

    test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! hellwedwdqo ffkkdwderewdsasasdlabasff");
+app.MapGet("/", () => "Hello World! hellwedwdqo fddfkkdwderewdsasasdlabasff");
 
 app.Run();
